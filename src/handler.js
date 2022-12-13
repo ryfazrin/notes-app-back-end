d3e1fd2e981167b61108dc6d375e985549f32768
ryfazrin <>
2022-12-13 17:05:32 +0700

    added: array notes in handler.js

--- src/handler.js
+++ src/handler.js
@@ -1,4 +1,5 @@
 const { nanoid } = require('nanoid');
+const notes = require('./notes');
 
 const addNoteHandler = (request, h) => {
   const { title, tags, body } = request.payload;
@@ -6,6 +7,11 @@ const addNoteHandler = (request, h) => {
   const id = nanoid(16);
   const createdAt = new Date().toISOString();
   const updatedAt = createdAt;
+
+  const newNote = {
+    title, tags, body, id, createdAt, updatedAt,
+  };
+  notes.push(newNote);
 };
 
 module.exports = { addNoteHandler };
