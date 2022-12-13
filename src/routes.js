fc0647484011a363565409d0220a0fec0d1941d0
ryfazrin <>
2022-12-13 17:34:34 +0700

    added: route DELETE /notes/(id)

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const {
   addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler,
+  deleteNoteByIdHandler,
 } = require('./handler');
 
 const routes = [
@@ -22,6 +23,11 @@ const routes = [
     method: 'PUT',
     path: '/notes/{id}',
     handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
   },
 ];
 
